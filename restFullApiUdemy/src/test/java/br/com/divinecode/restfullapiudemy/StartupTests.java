c035bb110702a214b31c395e6ecce59952f1f591
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-08-30 09:41:57 -0300

    primeiro commit

---
+++ restFullApiUdemy/src/test/java/br/com/divinecode/restfullapiudemy/StartupTests.java
@@ -0,0 +1,9 @@
+package br.com.divinecode.restfullapiudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+}
