80ed3f91447ba42ee336c43bc4b71f628f7c8039
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-09-04 16:35:17 -0300

    [BACKEND] - Implementação da serialização customizada do JSON.

--- restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/domain/person/personDTO/PersonDTO.java
+++ restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/domain/person/personDTO/PersonDTO.java
@@ -1,15 +1,22 @@
 package br.com.divinecode.restfullapiudemy.domain.person.personDTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
+
+//Altera a ordem das variáveis na hora da resposta das requisições
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
 public class PersonDTO {
 
     private static final long SerialVersionUID = 1L;
 
     private Long id;
 
+    //altera o nome da variável para resposta das requisições
+    @JsonProperty("first_name")
     private String firstName;
 
     private String lastName;
