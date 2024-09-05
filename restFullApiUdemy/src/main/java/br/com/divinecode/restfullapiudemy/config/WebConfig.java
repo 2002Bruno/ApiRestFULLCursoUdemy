a56830ee8e5a9b58c7495b0c8e9a0b2df4a8809b
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-09-05 09:56:16 -0300

    [BACKEND] - Implementação do content negociation via query parameter para retornar dois tipos de formato (JSON e XML).

---
+++ restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/config/WebConfig.java
@@ -0,0 +1,24 @@
+package br.com.divinecode.restfullapiudemy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    //Implementacao das configuracoes do content negotiation para retorno de json e xml
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.
+                favorParameter(true).
+                parameterName("mediaType").
+                ignoreAcceptHeader(true).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("json", MediaType.APPLICATION_JSON);
+    }
+}
--- restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/controller/PersonController.java
+++ restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/controller/PersonController.java
@@ -19,35 +19,39 @@ public class PersonController {
     private PersonService personService;
 
     //E necessario deixar a configuracao produces = MediaType.APPLICATION_JSON_VALUE para a integracao com o Swagger
-    @GetMapping(value = "/find-by-id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/find-by-id/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<PersonDTO> findById(@PathVariable(value = "id") Long id) {
         PersonDTO person = personService.findById(id);
 
         return ResponseEntity.ok(person);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<List<PersonDTO>> findAll() {
         List<PersonDTO> personList = personService.findAll();
 
         return ResponseEntity.ok().body(personList);
     }
 
-    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/create",
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO person) {
         PersonDTO newPerson = personService.create(person);
 
         return ResponseEntity.ok(newPerson);
     }
 
-    @PostMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/update",
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
     public ResponseEntity<PersonDTO> update(@RequestBody PersonDTO person) {
         PersonDTO newPerson = personService.update(person);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(newPerson);
     }
 
-    @DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         personService.delete(id);
 
