efa38df8050de44b81640e43d387973c3e4401c0
Bruno Mikael <brunno.rodrigues0706@@gmail.com>
2024-09-06 14:23:31 -0300

    [BACKEND] - Implementação do cors configuration para expecificar as rotas que tem permissão para consumir os endpoints da api.

--- restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/config/WebConfig.java
+++ restFullApiUdemy/src/main/java/br/com/divinecode/restfullapiudemy/config/WebConfig.java
@@ -1,14 +1,28 @@
 package br.com.divinecode.restfullapiudemy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatters = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatters.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
 
     //Implementacao das configuracoes do content negotiation para retorno de json e xml
     @Override
---
+++ restFullApiUdemy/src/test/java/br/com/divinecode/unittests/mapper/mocks/MockBook.java
@@ -0,0 +1,4 @@
+package br.com.divinecode.unittests.mapper.mocks;
+
+public class MockBook {
+}
